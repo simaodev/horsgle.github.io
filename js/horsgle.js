e8ac5ae686e57336675e6a9840e138217d851ef0
William Felipe Simão <wfsimao@gmail.com>
2017-07-21 15:31:30 -0300

    alterado layout

--- js/horsgle.js
+++ js/horsgle.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('.parallax').parallax();
+    $('.carousel').carousel('next', 3);
+    $('.carousel').carousel('set', 4);
+    $('.tap-target').tapTarget('open');
+    $('.tap-target').tapTarget('close');
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      duration: 2000
+    });
+
 });
