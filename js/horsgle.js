d068e3cadc6dface53dd1639efeffd43953db1ef
William Felipe Simão <wfsimao@gmail.com>
2018-06-26 23:13:22 -0300

    alterações

--- js/horsgle.js
+++ js/horsgle.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
     $('.parallax').parallax();
-    $('.carousel').carousel('next', 3);
-    $('.carousel').carousel('set', 4);
+    // $('.carousel').carousel('next', 3);
+    // $('.carousel').carousel('set', 4);
     $('.tap-target').tapTarget('open');
     $('.tap-target').tapTarget('close');
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      duration: 2000
+    // $('.carousel.carousel-slider').carousel({
+    //   fullWidth: true,
+    //   duration: 2000
+    // });
+    // $('.carousel').carousel(
+    //   {
+    //     dist: 0,
+    //     padding: 0,
+    //     fullWidth: true,
+    //     indicators: true,
+    //     duration: 100,
+    //   }
+    // );
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+      loop:true,
+      autoplay:true,
+      smartSpeed: 1000,
+      autoplayTimeout:4000,
+      autoWidth:true
     });
-
+    
+    $(".button-collapse").sideNav();
 });
+// autoplay()
+// function autoplay() {
+//     $('.carousel').carousel('next');
+//     setTimeout(autoplay, 4500);
+// }
